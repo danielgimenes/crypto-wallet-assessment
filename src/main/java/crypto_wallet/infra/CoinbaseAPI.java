d3b53dc24c886827033d90d8c3d918239893122e
Daniel Gimenes <danielcgimenes@gmail.com>
2023-05-08 17:44:57 -0300

    Adding fixed pool parallelism in CoinbaseAPI

--- src/main/java/crypto_wallet/infra/CoinbaseAPI.java
+++ src/main/java/crypto_wallet/infra/CoinbaseAPI.java
@@ -5,17 +5,48 @@ import crypto_wallet.domain.PriceAPI;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
 import java.util.function.Function;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 public class CoinbaseAPI implements PriceAPI {
+
+    private final Logger logger = Logger.getLogger(CoinbaseAPI.class.getName());
+
+    private final ForkJoinPool threadPool = new ForkJoinPool(3);
+
     @Override
     public Map<String, BigDecimal> fetchPrices(List<String> symbols) {
-        return symbols.stream().collect(
-                Collectors.toMap(Function.identity(), this::fetchAssetPrice));
+        try {
+            return threadPool.submit(() -> symbols.parallelStream().collect(
+                    Collectors.toMap(Function.identity(), this::fetchAssetPrice))).get();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
     }
 
+    Map<String, BigDecimal> fakePrices = Map.of(
+            "BTC", new BigDecimal("10.00"),
+            "ETH", new BigDecimal("20.00"),
+            "USDT", new BigDecimal("30.00"),
+            "ADA", new BigDecimal("40.00"),
+            "XRP", new BigDecimal("50.00"),
+            "DOGE", new BigDecimal("42.00")
+    );
+
     private BigDecimal fetchAssetPrice(String symbol) {
-        return new BigDecimal("10.00");
+        logger.info(String.format("[%s, start] fetching price of '%s'", Thread.currentThread().getName(), symbol));
+        long start = System.currentTimeMillis();
+        try {
+            Thread.sleep((int) (2000.0*Math.random()));
+        } catch (InterruptedException ignored) {}
+        BigDecimal price = fakePrices.get(symbol);
+        long elapsed = System.currentTimeMillis() - start;
+        logger.info(String.format("[%s, start] got '%s' price of %s after %d ms", Thread.currentThread().getName(), symbol, price.toString(), elapsed));
+        return price;
     }
 }
--- src/test/java/crypto_wallet/infra/CoinbaseAPITests.java
+++ src/test/java/crypto_wallet/infra/CoinbaseAPITests.java
@@ -12,12 +12,16 @@ public class CoinbaseAPITests {
 
     @Test
     public void fetchPrices() {
-        List<String> symbols = List.of("BTC", "ETH");
+        List<String> symbols = List.of("BTC", "ETH", "USDT", "ADA", "XRP", "DOGE");
         CoinbaseAPI api = new CoinbaseAPI();
 
         Map<String, BigDecimal> expected = Map.of(
                 "BTC", new BigDecimal("10.00"),
-                "ETH", new BigDecimal("20.00")
+                "ETH", new BigDecimal("20.00"),
+                "USDT", new BigDecimal("30.00"),
+                "ADA", new BigDecimal("40.00"),
+                "XRP", new BigDecimal("50.00"),
+                "DOGE", new BigDecimal("42.00")
         );
         assertEquals(expected, api.fetchPrices(symbols));
     }
